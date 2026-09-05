3b1631d031139e4a13a5092ee93a9da03f746f10
SHOWDER-png <vararysacare07@gmail.com>
2026-09-05 21:54:38 +0700

    Update Crypto888 UI and trading features

---
+++ .widget_preview/lib/src/dtd/dtd_connection_info.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: implementation_imports
+
+const String kWidgetPreviewDtdUri = 'ws://127.0.0.1:50930/hRXefGi_JP8=';
+const String kWidgetPreviewService =
+    'widget-preview-cd29915a-06e1-4af9-bc34-5fbe5ff3fa4d';
+const String kWidgetPreviewScaffoldStream =
+    'WidgetPreviewScaffold-cd29915a-06e1-4af9-bc34-5fbe5ff3fa4d';
+const String kProjectRootPath = r'D:\Kosen\trade_der';
